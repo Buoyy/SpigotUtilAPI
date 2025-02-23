5a47760880c6a3e8cdb57ab5977ae911b3fc980e
Buoyy <saarthak0902@gmail.com>
2025-02-23 13:58:51 +0500

    Add currency names to CurrencyType

--- src/main/java/com/github/buoyy/api/CurrencyType.java
+++ src/main/java/com/github/buoyy/api/CurrencyType.java
@@ -3,14 +3,27 @@ package com.github.buoyy.api;
 import org.bukkit.Material;
 
 public enum CurrencyType {
-    DIAMOND(Material.DIAMOND),
-    GOLD(Material.GOLD_INGOT);
+    DIAMOND(Material.DIAMOND, "diamond", "diamonds"),
+    GOLD(Material.GOLD_INGOT, "gold", "gold");
+
     private final Material material;
-    CurrencyType(Material material) {
+    private final String nameSingular;
+    private final String namePlural;
+    CurrencyType(Material material, String nameSingular, String namePlural) {
         this.material = material;
+        this.nameSingular = nameSingular;
+        this.namePlural = namePlural;
     }
 
     public Material getMaterial() {
         return material;
+    }
+
+    public String getNameSingular() {
+        return nameSingular;
+    }
+
+    public String getNamePlural() {
+        return namePlural;
     }
 }
